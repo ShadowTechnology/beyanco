94f3cb9e5246130ca9020d010c5975261ca84644
Vinothkumar <vinoth3941@gmail.com>
2025-11-22 11:44:25 +0530

    UI

--- src/app/components/project-card/project-card.component.ts
+++ src/app/components/project-card/project-card.component.ts
@@ -6,7 +6,7 @@ import { Component, Input } from '@angular/core';
   standalone: true,
   imports: [CommonModule],
   templateUrl: './project-card.component.html',
-  styleUrl: './project-card.component.scss'
+  styleUrl: './project-card.component.css'
 })
 export class ProjectCardComponent {
   @Input() project: any;
