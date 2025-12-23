3299723501513ecaa24843a0d50efd7c168a7b0d
Vinothkumar <vinoth3941@gmail.com>
2025-12-23 10:18:08 +0530

    See Samples before/after

--- src/app/components/staging-showcase/staging-showcase.component.ts
+++ src/app/components/staging-showcase/staging-showcase.component.ts
@@ -11,7 +11,7 @@ import { RouterLink } from "@angular/router";
 })
 export class StagingShowcaseComponent {
 
-  tabs = ['Living Rooms', 'Bedrooms', 'Dining Rooms', 'Offices', 'Exteriors', '360°'];
+  tabs = ['Living Rooms', 'Bedrooms', 'Dining Rooms', 'Offices'];
   activeTab = 'Living Rooms';
 
   viewMode: 'before' | 'after' = 'after';
@@ -20,11 +20,16 @@ export class StagingShowcaseComponent {
   // Images per tab
   gallery: any = {
     'Living Rooms': [
-      { before: 'assets/images/before1.png', after: 'assets/images/before2.png' },
-      { before: 'assets/images/after2.png', after: 'assets/images/after1.png' }
+      { before: 'assets/beforeimages/A1.jpg', after: 'assets/afterimages/A1-1.png' }
     ],
     'Bedrooms': [
-      { before: 'assets/images/after2.png', after: 'assets/images/after1.png' }
+      { before: 'assets/beforeimages/A4.jpg', after: 'assets/afterimages/A4-4.png' }
+    ],
+    'Dining Rooms': [
+      { before: 'assets/beforeimages/A9.jpg', after: 'assets/afterimages/A9-9.jpg' }
+    ],
+    'Offices': [
+      { before: 'assets/beforeimages/A7.jpg', after: 'assets/afterimages/A7-7.jpg' }
     ]
   };
 
